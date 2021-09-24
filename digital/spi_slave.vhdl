3149daa60c765d7f0b8aaa87d54bccf4a68d384a
daniel-santos-7 <danielsantos7@outlook.com.br>
2021-09-24 13:25:39 -0300

    internal register rename

--- digital/spi_slave.vhdl
+++ digital/spi_slave.vhdl
@@ -26,7 +26,7 @@ end entity spi_slave;
 
 architecture spi_slave_arch of spi_slave is
 
-    signal buffer_reg: std_logic_vector(WORD-1 downto 0);
+    signal shift_reg: std_logic_vector(WORD-1 downto 0);
 
 begin
 
@@ -35,16 +35,16 @@ begin
 
         if reset = '1' then
             
-            buffer_reg <= (others => '0');
+            shift_reg <= (others => '0');
 
         elsif rising_edge(sclk) and ss = '0' then
 
-            buffer_reg <= buffer_reg(WORD-2 downto 0) & sdi; 
+            shift_reg <= shift_reg(WORD-2 downto 0) & sdi; 
 
         end if;            
 
     end process rx;
 
-    data <= buffer_reg;
+    data <= shift_reg;
     
 end architecture spi_slave_arch;
